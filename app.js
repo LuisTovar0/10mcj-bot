23267a3743b09f22e55dfd694d20eff1a56de754
Luís Tovar <1181585@isep.ipp.pt>
2022-03-14 21:12:05 +0000

    Attempting to use botgram

--- app.js
+++ app.js
@@ -8,20 +8,20 @@ const adminChatId = process.env.ADMIN_CHAT_ID;
 if (!botToken || !adminChatId) throw new Error('BOT_TOKEN or ADMIN_CHAT_ID environment variables may not be defined.');
 else console.log('your bot token: ' + botToken + '\nthe admin\'s chat ID: ' + adminChatId);
 
-// const bot = botgram(botToken);
-//
-// bot.command("start", function (msg, reply, next) {
-//   reply.text('Bem-vindo');
-// });
-//
-// bot.message(function (msg, reply, next) {
-//   reply.text("You said:");
-//   try {
-//     reply.message(msg);
-//   } catch (err) {
-//     reply.text("Couldn't resend that.");
-//   }
-// });
+const bot = botgram(botToken);
+
+bot.command("start", function (msg, reply, next) {
+  reply.text('Bem-vindo');
+});
+
+bot.message(function (msg, reply, next) {
+  reply.text("You said:");
+  try {
+    reply.message(msg);
+  } catch (err) {
+    reply.text("Couldn't resend that.");
+  }
+});
 
 const express = require('express');
 const app = express();
@@ -43,12 +43,12 @@ function sendTextToTelegram(chatId, message) {
   requestmaker(response);
 }
 
-app.post('/' + botToken, function (request, response) {
-  response.set('Content-Type', 'application/json');
-  console.log(request.body);
-  const userText = request.body.message.text;
-  const chatId = request.body.message.chat.id;
-  sendTextToTelegram(chatId, userText);
-  sendTextToTelegram(chatId, 'lol 😂');
-  response.end();
-});
+// app.post('/' + botToken, function (request, response) {
+//   response.set('Content-Type', 'application/json');
+//   console.log(request.body);
+//   const userText = request.body.message.text;
+//   const chatId = request.body.message.chat.id;
+//   sendTextToTelegram(chatId, userText);
+//   sendTextToTelegram(chatId, 'lol 😂');
+//   response.end();
+// });
