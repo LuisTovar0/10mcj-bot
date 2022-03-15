1bae2c50d11bff16cf61954573811e404ffb8773
Luís Tovar <1181585@isep.ipp.pt>
2022-03-15 11:35:53 +0000

    Server version, perfected text formatting

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import {readFile} from 'fs';
 import express from 'express';
 import dotenv from 'dotenv';
 import request from 'request';
@@ -5,7 +6,13 @@ import botgram from 'botgram';
 
 // have a server just to show the site and have Heroku host us
 const app = express();
-app.use('/', express.static('./'));
+app.get('/', (req, res) =>
+  readFile('./index.html', 'utf8', (err, html) => {
+    if (err) res.status(500).send('Sorry, out of order');
+    res.send(html);
+  })
+);
+app.listen(process.env.PORT || 15000, () => sendMessage(adminChatId, "Site is up"));
 
 // the env vars
 if (!dotenv.config()) throw new Error('Could not find .env file!');
@@ -18,16 +25,30 @@ else console.log('your bot token: ' + botToken + '\nthe admin\'s chat ID: ' + ad
 // run the bot
 const bot = botgram(botToken);
 bot.message((msg, reply, next) => {
-  let split = msg.text.split('\n');
-  if (split.length < 3) {
-    reply.text('bad message');
-    return;
-  }
-  let url = split[0], desc = split[2],
-    date = new Date(), month = date.getMonth();
+  try {
+    let split = msg.text.split('\n');
+    if (split.length < 3) {
+      reply.text('bad message: should have 3 paragraphs');
+      return;
+    }
+    let url = split[0].trim(), desc = split[2].replace('*', '').replace('*', '').trim(),
+      date = new Date(), day = date.getDay(), month = date.getMonth() + 1;
+    if (date.getHours() > 10) {
+      let tomorrow = new Date();
+      tomorrow.setDate(date.getDate() + 1);
+      day = tomorrow.getDate();
+    }
 
-  reply.markdown(date.getDate() + '-' + (month < 10 ? '0' + month : month) + '-' + date.getFullYear().toString().substring(2, 4) +
-    '\n\n__' + desc + '__\n\n[▶️ YouTube](' + url + ')          [🌐 +Info](https://10minutoscomjesus.org/)');
+    let textToSend = day + '-' + (month < 10 ? '0' : '') + month + '-' + date.getFullYear().toString().substring(2, 4) +
+      '\n\n*' + desc + '*\n\n[▶️ YouTube](' + url + ')          [🌐 +Info](https://10minutoscomjesus.org/)';
+    reply.markdown(textToSend);
+    console.log(textToSend);
+  } catch (e) {
+    let textToSend = 'Olha só para isto!\n' +
+      '```\n' + e.stack + '\n```\nAgora vais sentar-te e pensar no que fizeste..!';
+    reply.markdown(textToSend);
+    console.log(textToSend);
+  }
 });
 
 function sendMessage(chatId, message) {
