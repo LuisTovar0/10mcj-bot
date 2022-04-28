67bbfbcde833887d889339e4fe87a09f8220b0d8
Luís Tovar <72473883+LuisTovar0@users.noreply.github.com>
2022-04-28 10:52:11 +0100

    Small test for the auto-deployment

--- src/app.ts
+++ src/app.ts
@@ -44,3 +44,6 @@ runBot(botToken, adminChatId, runningEnv);
 
 // inform me that it's running
 sendMessage(adminChatId, 'Bot is running in ' + runningEnv);
+
+// for testing if auto deploy is working
+sendMessage(adminChatId, `Auto-deploy worked`);
