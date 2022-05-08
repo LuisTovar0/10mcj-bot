d607238c8c250ba8457078920aaa7d4ccc640768
Luís Tovar <72473883+LuisTovar0@users.noreply.github.com>
2022-05-08 23:12:37 +0100

    Fixing the small text formatting fix

--- src/textFormatting.ts
+++ src/textFormatting.ts
@@ -24,10 +24,10 @@ https://youtu.be/dQw4w9WgXcQ\n\n\u{23F9}\u{1F649} *Desliguem o funk que eu não 
   const formattedDate = `${day}-${(month < 10 ? `0` : ``) + month}-${date.getFullYear().toString().substring(2, 4)}`;
 
   const telegramStr = `${formattedDate}\n\n*${descr1}*${descr2 ? `\n_${descr2}_` : ``}\n\n[\u{25B6} YouTube](${url})\
-          [\u{1F310} +Info](https://10minutoscomjesus.org/)`;
+          [\u{1F310} +Info](https://t.me/dezmincomjesus/424)`;
   const signalStr = `${formattedDate}\n\n${descr1}${descr2 ? `\n\n` + descr2 : ``}\n\n\u{25B6} YouTube: ${url}\n\n\u{1F4F2} \
 App 10 Minutos com Jesus. Disponível em:\n\u{1F34E} App Store - https://tinyurl.com/10mcj-ios\n\u{1F47E} Google Play - \
-https://tinyurl.com/10mcj-android\n\n\u{1F310} +Info: https://t.me/dezmincomjesus/424`;
+https://tinyurl.com/10mcj-android\n\n\u{1F310} +Info: https://10minutoscomjesus.org/`;
 
   return {telegram: telegramStr, signal: signalStr, date: formattedDate, descr: descr1};
 }
